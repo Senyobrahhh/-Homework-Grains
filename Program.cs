431329d28f39cf642fa7f8becff3d593ac93ec7a
Enyo <senyobrah@gmail.com>
2024-11-25 00:40:47 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Grains;
+
+class Program
+{
+    public static ulong GrainsOnSquare(int square)
+    {
+        if (square < 1 || square > 64)
+        {
+            throw new ArgumentOutOfRangeException(nameof(square), "Square must be between 1 and 64");
+        }
+        return 1UL << (square - 1);
+    }
+
+    public static ulong TotalGrainsOnBoard()
+    {
+        return ulong.MaxValue;
+    }
+    
+    static void Main(string[] args)
+    {
+        int square = GetSquareInput();
+
+        ulong grainsOnSquare = GrainsOnSquare(square);
+        ulong totalGrains = TotalGrainsOnBoard();
+        
+        Console.WriteLine($"Grains on square {square}: {totalGrains}");
+        Console.WriteLine($"Total grains on the chessboard: {totalGrains}");
+    }
+
+    static int GetSquareInput()
+    {
+        int square;
+        while (true)
+        {
+            Console.Write("Enter a square number (1-64): ");
+
+            if (int.TryParse(Console.ReadLine(), out square) && square >= 1 && square <= 64)
+            {
+                return square;
+            }
+            
+            Console.WriteLine("Invalid input. Try again.");
+        }
+    }
+}
